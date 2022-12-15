d9a2c77cf4eb2407433839acf02b7040c3242eb4
Лидия Турсунова <vivapomba@gmail.com>
2022-12-15 21:27:24 +0300

    vasya skasal

---
+++ vasya.R
@@ -0,0 +1,18 @@
+oop <- c(r$allele_names)
+loop <- c(oop[[1]], oop[[2]],oop[[3]],oop[[4]],oop[[5]])
+length(loop)
+
+  oop <- c(r$nalleles)
+loop <- c(oop[[1]], oop[[2]],oop[[3]],oop[[4]],oop[[5]])
+length(loop)
+
+oop <- c(r$)
+loop <- c(oop[[1]], oop[[2]],oop[[3]],oop[[4]],oop[[5]])
+length(loop)
+
+
+ oop <- c(r$allele_names)
+View(oop)
+ri <- divBasic('dataf2.gen')
+View(ri)
+ri[["HWE"]]
